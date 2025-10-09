9f76dd227ced1049f8dfef7788bdac82172fda39
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-10-09 23:46:55 +0530

    maze

--- dotgame/src/app/page.tsx
+++ dotgame/src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import LoginButton from '@/components/LoginButton'
 import React from 'react'
-
+import MazeGame from '@/components/MazeGame';
 const page = () => {
   return (
-    <LoginButton/>
+    <> <LoginButton /><main className="flex min-h-screen items-center justify-center bg-slate-900 text-white">
+      <MazeGame />
+    </main></>
   )
 }
 
